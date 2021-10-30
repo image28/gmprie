6d1223bb312ed4840b08d6c19813dba1f838d3be
Kevin Macey <7416366+image28@users.noreply.github.com>
2021-10-31 12:40:18 +1300

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -1,4 +1,4 @@
-CONNECT="POST / HTTP/1.1\r\nHost:127.0.0.1:28332\r\nAuthorization: Basic aW1hZ2U6eA==\r\nAccept: */*\r\nContent-Length: 74\r\nContent-Type: application/x-www-form-urlencoded\r\n\r\n{\"method\": \"getblocktemplate\", \"params\": [{\"rules\":[\"segwit\"]}], \"id\": 0}\r\n"
+CONNECT="POST / HTTP/1.1\r\nHost:127.0.0.1:28332\r\nAuthorization: Basic **********\r\nAccept: */*\r\nContent-Length: 74\r\nContent-Type: application/x-www-form-urlencoded\r\n\r\n{\"method\": \"getblocktemplate\", \"params\": [{\"rules\":[\"segwit\"]}], \"id\": 0}\r\n"
 SUBMIT=""
 
 # Connect and get work
