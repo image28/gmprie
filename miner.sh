579478ad198483e1d2e43e3bea6f7b375a29cd44
Kevin Macey <7416366+image28@users.noreply.github.com>
2021-10-31 13:15:22 +1300

    Started filling in prototype miner
    Will replace with faster, lower latency c code later on after I've tested the procedure with this code first.

--- miner.sh
+++ miner.sh
@@ -1,9 +1,21 @@
 CONNECT="POST / HTTP/1.1\r\nHost:127.0.0.1:28332\r\nAuthorization: Basic **********\r\nAccept: */*\r\nContent-Length: 74\r\nContent-Type: application/x-www-form-urlencoded\r\n\r\n{\"method\": \"getblocktemplate\", \"params\": [{\"rules\":[\"segwit\"]}], \"id\": 0}\r\n"
-SUBMIT=""
+SUBMIT="POST / HTTP/1.1\r\nHost:127.0.0.1:28332\r\nAuthorization: Basic **********\r\nAccept: */*\r\nContent-Length: BLOCKLENGTH\r\nContent-Type: application/x-www-form-urlencoded\r\n\r\n{\"method\": \"submitblock\", \"params\": [{\"OUTPUT\"}], \"id\": BLOCKID}\r\n"
+#THREADS=2
+INTERVAL=100000000000
+DATE=`date +%j.%Y`
 
 # Connect and get work
-echo -ne "$CONNECT" | netcat -q10 127.0.0.1 28332
+echo -ne "$CONNECT" | netcat -q10 127.0.0.1 28332 > .worklog-$DATE
+# Calculate difficulty
+DIFF=`cat .worklog-$DATE` # unfinished
+BLOCKID=`cat .worklog-$DATE`
+LIMIT=$((DIFF+INTERVAL))
 # Mine a block
+OUTPUT=`./gmp_prime $DIFF $LIMIT`
 
 # Submit the block
-
+echo -ne "$SUBMIT" | \
+    sed -e s/"BLOCKID"/"$BLOCKID"/ | \
+    sed -e s/"BLOCKLENGTH"/"$BLOCKLENGTH"/ | \
+    sed -e s/"OUTPUT"/"$OUTPUT"/ | \
+    netcat -q10 127.0.0.1 28332
