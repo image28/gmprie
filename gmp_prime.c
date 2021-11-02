828c6b3ed139e401c5dfb6f1f3e3e300a77bbff0
Kevin Macey <7416366+image28@users.noreply.github.com>
2021-11-02 14:00:56 +1300

    Added check for 3300

--- gmp_prime.c
+++ gmp_prime.c
@@ -1,6 +1,7 @@
 // #gcc gmp_prime.c -O3 -lgmp -lm -o gmp_prime
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <unistd.h>
 #include <time.h>
 #include <sys/wait.h>
@@ -32,7 +33,7 @@ int main(int argc, char *argv[])
     int ismanager=0;
     int i=0;
     int e=0;
-
+    uint8_t byte=0;
 
     if ( argc < 2)
         exit(1);
@@ -50,52 +51,68 @@ int main(int argc, char *argv[])
     mpz_add(start,start,range);
     mpz_add(range,range,start);
 
-    while( mpz_cmp(res,range) <  0)
+    while(1) // mpz_cmp(res,range) <  0)
     {
-        mpz_set(next,start);
         mpz_nextprime (res, start);
-        mpz_add_ui(next,next,check[cur]);
-        //gmp_printf("%#Zu\n%#Zu\n\n",res,next);
-
-        if ( mpz_cmp(res,next) == 0 )
-        {
-        #ifdef DEBUG
-            printf(".");
-        #endif
-            cur++;
-        }else{
-            cur=0;
-        }
-
+        mpz_set(next,res);
+        // mpz_export (void *rop, size_t *countp, int order, size_t size, int endian, size_t nails, const mpz_t op)
+        mpz_mod_ui(next,next,100);
+        mpz_export (&byte, NULL, -1, 1, 0, 0, next);
+        //gmp_printf("%u %#Zd\n",byte,res);
         mpz_set(next,start);
-        mpz_add_ui(next,next,check2[cur2]);
 
-        if ( mpz_cmp(res,next) == 0 )
+        /*  for e in `primesieve -p3 1 900000 | awk -F',' '{print $1}' |cut -c 2- |rev | cut -c1-2 | rev | sort -g | uniq | grep -Ev "^.$" | sed -e s/"^0"/""/`;
+            do
+                echo -en "( byte%100 == $e ) || ";
+            done > primetuples-3300.gmprie */
+        if ( ( byte%100 == 1 ) || ( byte%100 == 3 ) || ( byte%100 == 7 ) || ( byte%100 == 11 ) || ( byte%100 == 13 ) || ( byte%100 == 17 ) || ( byte%100 == 21 ) || ( byte%100 == 23 ) || ( byte%100 == 27 ) || ( byte%100 == 31 ) || ( byte%100 == 33 ) || ( byte%100 == 37 ) || ( byte%100 == 41 ) || ( byte%100 == 43 ) || ( byte%100 == 47 ) || ( byte%100 == 51 ) || ( byte%100 == 53 ) || ( byte%100 == 57 ) || ( byte%100 == 61 ) || ( byte%100 == 63 ) || ( byte%100 == 67 ) || ( byte%100 == 71 ) || ( byte%100 == 73 ) || ( byte%100 == 77 ) || ( byte%100 == 81 ) || ( byte%100 == 83 ) || ( byte%100 == 87 ) || ( byte%100 == 91 ) || ( byte%100 == 93 ) || ( byte%100 == 97 ) )
         {
-        #ifdef DEBUG
-            printf(".");
-        #endif
-            cur2++;
-        }else{
-        #ifdef DEBUG
-            if ( cur2 > 0 )
+            mpz_add_ui(next,next,check[cur]);
+            //gmp_printf("%#Zu\n%#Zu\n\n",res,next);
+
+            if ( mpz_cmp(res,next) == 0 )
             {
-                printf("\n");
+            #ifdef DEBUG
+                printf(".");
+            #endif
+                cur++;
+            }else{
+                cur=0;
             }
-        #endif
-            cur2=0;
-        }
 
-        if ( ( cur == 6 ) || ( cur2 == 6 ) )
-        {
-            gmp_printf("BLOCK:%#Zx\n",res);
-            exit(0);
+            mpz_set(next,start);
+            mpz_add_ui(next,next,check2[cur2]);
+
+            if ( mpz_cmp(res,next) == 0 )
+            {
+            #ifdef DEBUG
+                printf(".");
+            #endif
+                cur2++;
+            }else{
+            #ifdef DEBUG
+                if ( cur2 > 0 )
+                {
+                    printf("\n");
+                }
+            #endif
+                cur2=0;
+            }
+
+            if ( ( cur == 6 ) || ( cur2 == 6 ) )
+            {
+                gmp_printf("BLOCK:%#Zd\n",res);
+                //exit(0);
+                cur=0;
+                cur2=0;
+            }
         }
 
         mpz_set(start,res);
+
     }
 
-    if ( ( ismanager ) && ( count == THREADS ) )
+    /*if ( ( ismanager ) && ( count == THREADS ) )
     {
 
         for(i=0; i < THREADS; i++)
@@ -114,9 +131,11 @@ int main(int argc, char *argv[])
         }
         printf("\n");
         waitpid(-1,NULL,0);
-    }
+    }*/
 
-    gmp_printf("Limit Reached %#Zd\nThread %d exited\n\n",res,count);
+    //gmp_printf("Limit Reached %#Zd\nThread %d exited\n\n",res,count);
+
+    waitpid(-1,NULL,0);
 
     return(0);
 }
